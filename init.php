e52c2dc62445bc0b5e879c136a02468aa02ab858
Ragen24 <stalker.20122@yandex.ru>
2017-09-05 23:43:29 +0300

    Add Basic and Premium customers

--- init.php
+++ init.php
@@ -1,7 +1,44 @@
 <?php
-require_once __DIR__ . '/Book.php';
-require_once __DIR__ . '/Customer.php';
-$book1 = new Book("1984", "George Orwell", 9785267006323, 12);
-$book2 = new Book("To Kill a Mockingbird", "Harper Lee", 9780061120084, 2);
-$customer1 = new Customer(1, 'John', 'Doe', 'johndoe@mail.com');
-$customer2 = new Customer(2, 'Mary', 'Poppins', 'mp@mail.com');
+
+use Bookstore\Domain\Customer\CustomerFactory;
+use Bookstore\Utils\Config;
+
+function __autoload($classname) {
+    $lastSlash = strpos($classname, '\\') + 1;
+    $classname = substr($classname, $lastSlash);
+    $directory = str_replace('\\', '/', $classname);
+    $filename = __DIR__ . '/src/' . $directory . '.php';
+    require_once($filename);
+}
+    CustomerFactory::factory('basic', 2, 'mary', 'poppins',
+        'mary@poppins.com');
+    CustomerFactory::factory('premium', 3, 'james', 'bond',
+        'james@bond.com');
+
+    $config = Config::getInstance();
+    $dbConfig = $config->get('db');
+
+
+    $addTaxes = function (array &$book, $index, $percentage) {
+        $book['price'] += round($percentage * $book['price'], 2);
+    };
+
+    $books = [
+        ['title' => '1984', 'price' => 8.15],
+        ['title' => 'Don Quijote', 'price' => 12.00],
+        ['title' => 'Odyssey', 'price' => 3.55]
+    ];
+    array_walk($books, $addTaxes, 0.16);
+    var_dump($books);
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bookstore init</title>
+    </head>
+    <body>
+        <p>sdgsdgsdg</p>
+    </body>
+</html>
--- src/Domain/Person.php
+++ src/Domain/Person.php
@@ -1,33 +1,38 @@
 <?php
-class Customer {
-    private $id;
-    private $firstname;
-    private $surname;
-    private $email;
+
+namespace Bookstore\Domain;
+
+use Bookstore\Utils\Unique;
+
+class Person {
+
+    use Unique;
+
+    protected $firstname;
+    protected $surname;
+    protected $email;
+
     public function __construct(
         int $id,
         string $firstname,
         string $surname,
         string $email
     ) {
-        $this->id = $id;
         $this->firstname = $firstname;
         $this->surname = $surname;
         $this->email = $email;
+        $this->setId($id);
     }
-    public function getID(): id {
-        return $this->id;
-    }
+
     public function getFirstname(): string {
         return $this->firstname;
     }
+
     public function getSurname(): string {
         return $this->surname;
     }
+
     public function getEmail(): string {
         return $this->email;
-    }
-    public function setEmail(string $email) {
-        $this->email = $email;
     }
 }
